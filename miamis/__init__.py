29955ef410941d8b6e44ed16b2c9c1fffd8e80fe
Anthony Soulain <anthony.soulain@gmail.com>
2020-06-26 11:14:57 +1000

    Add analysis tools.

--- miamis/__init__.py
+++ miamis/__init__.py
@@ -1,5 +1,7 @@
+from .analysis.easy_candid import fit_binary
 from .core import calibrate
+from .mf_pipeline.ami_function import make_mf
 from .mf_pipeline.bispect import extract_bs_mf
 from .oifits import cal2dict, data2obs, load, loadc, save, show
 
-__version__ = "0.1dev"
+__version__ = "0.2dev"
