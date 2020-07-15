5e6593ce8fea25bc9c52e5cd02086189611cd2d8
Anthony Soulain <anthony.soulain@gmail.com>
2020-07-15 16:47:19 +1000

    Add pymask repo to AMICAL.

---
+++ amical/analysis/pymask/__init__.py
@@ -0,0 +1,63 @@
+''' --------------------------------------------------------------------
+              PYMASK: Python aperture masking analysis pipeline
+    --------------------------------------------------------------------
+    ---
+    pymask is a python module for fitting models to aperture masking
+    data reduced to oifits format by the IDL masking pipeline.
+
+    It consists of a class, cpo, which stores all the relevant information
+    from the oifits file, and a set of functions, cp_tools, for manipulating
+    these data and fitting models.
+
+    Fitting is based on the MCMC Hammer algorithm (aka ensemble affine 
+    invariant MCMC) or the MultiNest algorithm (aka multimodal nested
+    sampling). Both of these must be installed correctly or else 
+    pymask won't work!
+    See readme.txt for more details.
+
+    - Ben
+    ---
+    -------------------------------------------------------------------- '''
+
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import astropy.io.fits as pf
+import copy
+import pickle
+import os
+import sys
+import pdb
+# import oifits
+
+shift = np.fft.fftshift
+fft   = np.fft.fft2
+ifft  = np.fft.ifft2
+
+dtor = np.pi/180.0
+
+# import .cp_tools
+from .cp_tools import *
+
+# import cpo
+# from cpo import *
+from .cpo import *
+
+# import pol_tools
+from .pol_tools import *
+
+# import x_y_plot
+from .x_y_plot import *
+
+# -------------------------------------------------
+# set some defaults to display images that will
+# look more like the DS9 display....
+# -------------------------------------------------
+#plt.set_cmap(cm.gray)
+(plt.rcParams)['image.origin']        = 'lower'
+(plt.rcParams)['image.interpolation'] = 'nearest'
+# -------------------------------------------------
+
+#plt.clf()
---
+++ amical/analysis/pymask/pol_tools.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  8 14:56:35 2016
+
+Tools for analysing and dealing with polarimetry data.
+Also able to read the outputs from the IDL pipeline
+
+@author: cheetham
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import astropy.io.fits as pf
+import copy
+import pickle
+import sys
+import pdb
+import glob
+import gzip
+import os, threading, subprocess
+import json
+from . import oifits
+import time
+import emcee
+from multiprocessing import Pool
+from scipy.optimize import leastsq
+from scipy.optimize import curve_fit
+import scipy.interpolate as interp
+
+from .cp_tools import rad2mas,mas2rad
+
+
+# =========================================================================
+def binary_model(u,v,wavel,p):
+    ''' Calculate the complex visibility observed by an array on a binary star
+    ----------------------------------------------------------------
+    p: 3-component vector (+2 optional), the binary "parameters":
+    - p[0] = sep (mas)
+    - p[1] = PA (deg) E of N.
+    - p[2] = contrast ratio (primary/secondary)
+    
+    optional:
+    - p[3] = angular size of primary (mas)
+    - p[4] = angular size of secondary (mas)
+
+    - u,v: baseline coordinates (meters)
+    - wavel: wavelength (meters)
+    ---------------------------------------------------------------- '''
+
+    p = np.array(p)
+    # relative locations
+    th = (p[1] + 90.0) * np.pi / 180.0
+    ddec =  mas2rad(p[0] * np.sin(th))
+    dra  = -mas2rad(p[0] * np.cos(th))
+
+    # baselines into number of wavelength
+    x = np.sqrt(u*u+v*v)/wavel
+
+    # decompose into two "luminosity"
+    l2 = 1. / (p[2] + 1)
+    l1 = 1 - l2
+    
+    # phase-factor
+    phi = np.zeros(u.size, dtype=complex)
+    phi.real = np.cos(-2*np.pi*(u*dra + v*ddec)/wavel)
+    phi.imag = np.sin(-2*np.pi*(u*dra + v*ddec)/wavel)
+
+    # optional effect of resolved individual sources
+    if p.size == 5:
+            th1, th2 = mas2rad(p[3]), mas2rad(p[4])
+            v1 = 2*j1(np.pi*th1*x)/(np.pi*th1*x)
+            v2 = 2*j1(np.pi*th2*x)/(np.pi*th2*x)
+    else:
+            v1 = np.ones(u.size)
+            v2 = np.ones(u.size)
+
+    cvis = l1 * v1 + l2 * v2 * phi
+    return cvis
+
+# =========================================================================
+
+def diff_vis_binary(u,v,wavel,p):
+    ''' The expected differential visibility signal assuming a model of the form:
+    separation, position angle, contrast.
+        This is just a really simple model that puts all of the complicated physics 
+    into the contrast term.
+        Contrast is really the polarization fraction with some factor due to the size
+    of the object, but they are completely degenerate so I merged them.'''
+    
+    # Get the expected signal from binary_model
+    binary_cvis=binary_model(u,v,wavel,p)
+    
+# =========================================================================
