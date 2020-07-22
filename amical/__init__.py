3693e812d400aece68f828858c8e6ddb2cbde160
Anthony Soulain <anthony.soulain@gmail.com>
2020-07-22 16:23:50 +1000

    Add the data cleaning in the __init__ of amical

--- amical/__init__.py
+++ amical/__init__.py
@@ -3,6 +3,7 @@ from .analysis.easy_pymask import pymaskGrid, pymaskCRlimit, pymaskMcmc
 from .core import calibrate
 from .mf_pipeline.ami_function import make_mf
 from .mf_pipeline.bispect import extract_bs
-from .oifits import cal2dict, data2obs, load, loadc, save, show
+from .oifits import cal2dict, load, loadc, save, show
+from .dataProcessing import selectCleanData
 
 __version__ = "0.3dev"
