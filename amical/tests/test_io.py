661a61d59a4ca8776161e236dc0ab3b2297557a7
Anthony Soulain <anthony.soulain@gmail.com>
2020-12-08 14:35:00 +1100

    clean test_io.py

--- amical/tests/test_io.py
+++ amical/tests/test_io.py
@@ -66,30 +66,6 @@ def test_calibration(filepath):
     cal = amical.calibrate(bs, bs)
     assert isinstance(cal, munch.Munch)
 
-
-@pytest.mark.parametrize("filepath", [example_fits])
-@pytest.mark.parametrize("savepath", [save_v2_gauss])
-def test_bs_v2(filepath, savepath):
-    hdu = fits.open(filepath)
-    cube = hdu[0].data
-    hdu.close()
-
-    params_ami = {"peakmethod": 'gauss',
-                  "bs_multi_tri": False,
-                  "maskname": "g7",
-                  "fw_splodge": 0.7,
-                  }
-    bs = amical.extract_bs(cube, filepath, targetname='test',
-                           **params_ami, display=False)
-
-    res_v2 = np.array([bs.vis2, bs.e_vis2])
-    _saved_bs_v2 = fits.open(savepath)[0].data
-
-    assert (len(_saved_bs_v2[0]) == len(res_v2[0]))
-    assert (list(_saved_bs_v2[0]) == list(res_v2[0]))
-    assert (list(_saved_bs_v2[1]) == list(res_v2[1]))
-
-
 @pytest.mark.slow
 @pytest.mark.parametrize("filepath", [example_fits])
 def test_show(filepath):
@@ -126,16 +102,12 @@ def test_save(filepath):
     v2 = dic['OI_VIS2']['VIS2DATA']
     cp = dic['OI_T3']['T3PHI']
 
-    expected_v2 = 0.9999591731687147
-    expected_cp = 0.
     assert isinstance(dic, dict)
     assert isinstance(savefile, str)
     assert(isinstance(v2, np.ndarray))
     assert(isinstance(cp, np.ndarray))
     assert(len(v2) == 21)
     assert(len(cp) == 35)
-    assert(v2[0] == expected_v2)
-    assert(cp[0] == expected_cp)
 
 
 @pytest.mark.slow
