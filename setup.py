d0189cc5863568e9dfda7def00a31f722ed99895
Clément Robert <cr52@protonmail.com>
2021-06-17 10:42:03 +0200

    migrate packaging metadata to setup.cfg

--- setup.py
+++ setup.py
@@ -1,25 +1,2 @@
-#!/usr/bin/env python
-
-from setuptools import find_packages, setup
-
-project_name = "amical"
-
-setup(
-    name=project_name,
-    version="1.2",
-    packages=find_packages(),
-    author='Anthony Soulain',
-    author_email='anthony.soulain@sydney.edu.au.com',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Professional Astronomers',
-        'Topic :: High Angular Resolution Astronomy :: Interferometry',
-        'Programming Language :: Python :: 3.7'
-    ],
-    install_requires=["matplotlib", "munch", "numpy", "emcee",
-                      "astropy", "scipy", "termcolor", "tqdm",
-                      "uncertainties", "astroquery",
-                      "corner", "h5py"],
-    extras_require={"dev": ["pytest"]},
-    package_data={"amical": ["internal_data/*.fits"]},
-)
+from setuptools import setup
+setup()
