edb28da021caefadda729a255b867038e9cfd168
Thomas Vandal <thomasvandal@hotmail.com>
2021-11-02 23:29:17 -0400

    Add tests for astropy version and commentary cards

--- amical/tests/test_extraction.py
+++ amical/tests/test_extraction.py
@@ -1,19 +1,51 @@
+import pytest
+import astropy
 import munch
 from astropy.io import fits
+from packaging.version import Version
 
 from amical.mf_pipeline.bispect import _add_infos_header
 
 
-def test_add_infos_simulated():
+@pytest.fixture()
+def infos():
+    # SimulatedData avoids requiring extra keys in infos
+    return munch.Munch(orig="SimulatedData", instrument="unknown")
+
+
+@pytest.fixture()
+def commentary_hdr():
+    # Create a fits header with commentary card
+    hdr = fits.Header()
+    hdr["HISTORY"] = "History is a commentary card"
+    return hdr
+
+@pytest.fixture()
+def astropy_versions():
+
+    astropy_version = astropy.__version__
+    working_version = "5.0.0"
+
+    return astropy_version, working_version
+
+
+
+@pytest.fixture
+def commentary_infos(infos, commentary_hdr):
+
+    # Add hdr to infos placeholders for everything but hdr
+    mf = munch.Munch(pixelSize=1.0)
+
+    return _add_infos_header(infos, commentary_hdr, mf, 1.0, "afilename", "amaskname", 1)
+
+
+def test_add_infos_simulated(infos):
     # Ensure that keys are passed to infos for simulated data, but only when available
 
     # Create a fits header with two keywords that are usually passed to infos
     hdr = fits.Header()
     hdr["DATE-OBS"] = "2021-06-23"
     hdr["TELESCOP"] = "FAKE-TEL"
-
-    # This test is for simulated data only
-    infos = munch.Munch(orig="SimulatedData", instrument="unknown")
 
     # Add hdr to infos placeholders for everything but hdr
     mf = munch.Munch(pixelSize=1.0)
@@ -22,21 +54,45 @@ def test_add_infos_simulated():
     assert infos["date-obs"] == hdr["DATE-OBS"]
     assert infos["telescop"] == hdr["TELESCOP"]
     assert "observer" not in infos  # Keys that are not in hdr should not be in infos
+    assert "observer" not in infos.hdr  # Keys that are not in hdr should still not be
 
 
-def test_add_infos_header_commentary():
+def test_add_infos_header_commentary(commentary_infos):
     # Make sure that _add_infos_header handles _HeaderCommentaryCards from astropy
 
-    # Create a fits header with commentary card
-    hdr = fits.Header()
-    hdr["HISTORY"] = "History is a commentary card"
+    # Convert everything to munch object
+    munch.munchify(commentary_infos)
 
-    # SimulatedData avoids requiring extra keys in infos
-    infos = munch.Munch(orig="SimulatedData", instrument="unknown")
+
+def test_commentary_infos_keep(commentary_infos, astropy_versions):
+    # Check that commentary cards are removed or kept depending on astropy version
+
+    astropy_version, working_version = astropy_versions
+
+    if Version(astropy_version) < Version(working_version):
+        assert "HISTORY" not in commentary_infos.hdr
+    else:
+        assert "HISTORY" in commentary_infos.hdr
+
+
+def test_astropy_version_warning(infos, commentary_hdr, astropy_versions, capfd):
+    # Test that AMICAL warns about astropy < 5.0 removing commentary cards
+
+    astropy_version, working_version = astropy_versions
 
     # Add hdr to infos placeholders for everything but hdr
     mf = munch.Munch(pixelSize=1.0)
-    infos = _add_infos_header(infos, hdr, mf, 1.0, "afilename", "amaskname", 1)
 
-    # Convert everything to munch object
-    munch.munchify(infos)
+    infos = _add_infos_header(infos, commentary_hdr, mf, 1.0, "afilename", "amaskname", 1)
+    captured = capfd.readouterr()
+
+    if Version(astropy_version) < Version(working_version):
+        # NOTE: Adding colors codes because output with cprint has them
+        msg = (
+            "\x1b[32mCommentary cards are removed from the header with astropy"
+            f" version < {working_version}. Your astropy version is"
+            f" {astropy_version}\x1b[0m\n"
+        )
+        assert captured.out == msg
+    else:
+        assert not captured.out
