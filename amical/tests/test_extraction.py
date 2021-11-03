036ff56aba98df1430b74c5e94afb3eb4e3fe994
Thomas Vandal <thomasvandal@hotmail.com>
2021-11-03 10:59:53 -0400

    Remove redundant skipif's and use xfail

--- amical/tests/test_extraction.py
+++ amical/tests/test_extraction.py
@@ -42,10 +42,13 @@ def test_add_infos_simulated():
     mf = munch.Munch(pixelSize=1.0)
     infos = _add_infos_header(infos, hdr, mf, 1.0, "afilename", "amaskname", 1)
 
+    # Check that we kept required keys
     assert infos["date-obs"] == hdr["DATE-OBS"]
     assert infos["telescop"] == hdr["TELESCOP"]
-    assert "observer" not in infos  # Keys that are not in hdr should not be in infos
-    assert "observer" not in infos.hdr  # Keys that are not in hdr should still not be
+
+    # Keys that are not in hdr should not be in infos or hdr
+    assert "observer" not in infos
+    assert "observer" not in infos.hdr
 
 
 @pytest.mark.filterwarnings("ignore: Commentary cards")
@@ -56,7 +59,7 @@ def test_add_infos_header_commentary(commentary_infos):
     munch.munchify(commentary_infos)
 
 
-@pytest.mark.skipif(
+@pytest.mark.xfail(
     ASTROPY_VERSION < Version("5.0rc"),
     reason="Munch cannot handle commentary cards for Astropy < 5.0",
 )
@@ -64,45 +67,9 @@ def test_commentary_infos_keep(commentary_infos):
     assert "HISTORY" in commentary_infos.hdr
 
 
-@pytest.mark.skipif(
-    ASTROPY_VERSION >= Version("5.0rc"),
-    reason="Munch can handle commentary cards for Astropy 5.0+",
-)
 @pytest.mark.xfail(
-    ASTROPY_VERSION < Version("5.0rc"),
-    reason="AMICAL removes commentary cards from header with Astropy < 5.0",
-)
-def test_commentary_infos_drops(commentary_infos):
-    assert "HISTORY" in commentary_infos.hdr
-
-
-@pytest.mark.skipif(
-    ASTROPY_VERSION >= Version("5.0rc"),
-    reason="There are no warnings raised for Astropy 5.0+",
-)
-def test_commentary_warning_astropy_version():
-
-    # Add hdr to infos placeholders for everything but hdr
-    mf = munch.Munch(pixelSize=1.0)
-
-    # SimulatedData avoids requiring extra keys in infos
-    infos = munch.Munch(orig="SimulatedData", instrument="unknown")
-
-    # Create a fits header with commentary card
-    hdr = fits.Header()
-    hdr["HISTORY"] = "History is a commentary card"
-
-    with pytest.warns(RuntimeWarning, match="Commentary cards"):
-        infos = _add_infos_header(infos, hdr, mf, 1.0, "afilename", "amaskname", 1)
-
-
-@pytest.mark.skipif(
-    ASTROPY_VERSION < Version("5.0rc"),
-    reason="Astropy < 5.0 should raise a warning for commentary cards",
-)
-@pytest.mark.xfail(
-    ASTROPY_VERSION >= Version("5.0rc"),
-    reason="AMICAL should not raise a warning for commentary cards with astropy 5.0+",
+    ASTROPY_VERSION > Version("5.0rc"),
+    reason="Astropy > 5.0 should not raise a warning for commentary cards",
 )
 def test_no_commentary_warning_astropy_version():
 
