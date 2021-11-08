ce4e2b911c799cbfd650e751130d983cfb7070e9
Thomas Vandal <thomasvandal@hotmail.com>
2021-11-08 10:14:53 -0500

    Add bad pixel tests

--- amical/tests/test_processing.py
+++ amical/tests/test_processing.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 
 import amical
 from amical.data_processing import clean_data
+from amical.data_processing import fix_bad_pixels
 from amical.data_processing import sky_correction
 
 
@@ -105,3 +106,100 @@ def test_clean(global_datadir):
     assert type(cube_clean) == np.ndarray
     assert im1.shape == cube_clean.shape
     assert im2.shape == cube_clean.shape
+
+
+def test_fix_bad_pixel_no_bad():
+    img_dim = 80
+    data = np.random.random((img_dim, img_dim))
+
+    no_bpix = fix_bad_pixels(data, np.zeros_like(data, dtype=bool))
+
+    assert np.all(data == no_bpix)
+
+
+def test_fix_one_bad_pixel():
+
+    img_dim = 80
+    data = np.random.random((img_dim, img_dim))
+
+    bad_ind = tuple(np.random.randint(0, high=img_dim, size=2))
+    data[bad_ind] = 1e5
+
+    bad_map = np.zeros_like(data, dtype=bool)
+    bad_map[bad_ind] = 1
+
+    no_bpix = fix_bad_pixels(data, bad_map)
+
+    assert no_bpix[bad_map] != data[bad_map]
+    assert np.all(no_bpix[~bad_map] == data[~bad_map])
+    assert 0.0 <= no_bpix[bad_ind] <= 1.0  # Because test data is random U(0, 1)
+
+
+@pytest.mark.usefixtures("close_figures")
+def test_clean_data_no_bmap_add_bad():
+    n_im = 5
+    img_dim = 80
+    data = np.random.random((n_im, img_dim, img_dim))
+
+    bad_ind = tuple(np.random.randint(0, high=img_dim, size=2))
+    bad_ind_3d = (slice(None),) + bad_ind
+    add_bad = [bad_ind[::-1]]
+
+    # Put value out of data bounds (0, 1) to make sure corrected != original
+    data[bad_ind_3d] = 1e5
+
+    # Test case with non-emtpy add_bad but empty add_bad
+    cleaned = clean_data(data, sky=False, apod=False, add_bad=add_bad)
+    nobpix_list = [
+        fix_bad_pixels(img, bad_map=np.zeros((img_dim, img_dim)), add_bad=add_bad)
+        for img in data
+    ]
+
+    assert np.all([cleaned[i] == nobpix_list[i] for i in range(data.shape[0])])
+    assert np.all(cleaned[bad_ind_3d] != data[bad_ind_3d])
+
+
+@pytest.mark.usefixtures("close_figures")
+def test_clean_data_bmap_2d():
+    n_im = 5
+    img_dim = 80
+    data = np.random.random((n_im, img_dim, img_dim))
+
+    bad_map = np.zeros((img_dim, img_dim), dtype=bool)
+    bad_map[tuple(np.random.randint(0, high=img_dim, size=2))] = 1
+
+    data[:, bad_map] = 1e5
+
+    # Test case with non-emtpy add_bad but empty add_bad
+    cleaned = clean_data(data, sky=False, apod=False, bad_map=bad_map)
+    nobpix_list = [fix_bad_pixels(img, bad_map=bad_map) for img in data]
+
+    assert np.all([cleaned[i] == nobpix_list[i] for i in range(data.shape[0])])
+    assert np.all(cleaned[:, bad_map] != data[:, bad_map])
+    assert np.all(cleaned[:, ~bad_map] == data[:, ~bad_map])
+
+
+@pytest.mark.usefixtures("close_figures")
+def test_clean_data_bmap_add_bad_2d():
+    n_im = 5
+    img_dim = 80
+    data = np.random.random((n_im, img_dim, img_dim))
+
+    new_bad_ind = tuple(np.random.randint(0, high=img_dim, size=2))
+    add_bad = [new_bad_ind[::-1]]
+
+    bad_map = np.zeros((img_dim, img_dim), dtype=bool)
+    bad_map[tuple(np.random.randint(0, high=img_dim, size=2))] = 1
+
+    # Test case with non-emtpy add_bad but empty add_bad
+    cleaned = clean_data(data, sky=False, apod=False, bad_map=bad_map, add_bad=add_bad)
+    nobpix_list = [
+        fix_bad_pixels(img, bad_map=bad_map, add_bad=add_bad) for img in data
+    ]
+
+    full_bad_map = bad_map.copy()
+    full_bad_map[new_bad_ind] = True
+
+    assert np.all([cleaned[i] == nobpix_list[i] for i in range(data.shape[0])])
+    assert np.all(cleaned[:, full_bad_map] != data[:, full_bad_map])
+    assert np.all(cleaned[:, ~full_bad_map] == data[:, ~full_bad_map])
