868a6a9377bdf0a37e51a26b463a97de88822247
Anthony <anthony.soulain@protonmail.com>
2021-11-08 15:42:54 +0100

    Add data test for CLI

--- amical/tests/conftest.py
+++ amical/tests/conftest.py
@@ -6,3 +6,8 @@ import pytest
 @pytest.fixture(scope="session")
 def global_datadir():
     return pathlib.Path(__file__).parent / "data"
+
+
+@pytest.fixture(scope="session")
+def cli_datadir():
+    return pathlib.Path(__file__).parent / "cli_data"
