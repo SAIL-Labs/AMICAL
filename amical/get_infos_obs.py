886a9d75891a6832f25b6eaa48323ff3737cf922
Anthony <anthony.soulain@protonmail.com>
2022-08-04 16:57:53 +0200

    Add a function to plot the wavelenght of IFS-SPHERE

--- amical/get_infos_obs.py
+++ amical/get_infos_obs.py
@@ -231,3 +231,61 @@ def get_pixel_size(ins):
     except KeyError:
         p = np.NaN
     return p
+
+
+def get_ifu_table(i_wl, filtname="YH", instrument="SPHERE-IFS"):
+    """Get spectral information for the given instrumental IFU setup.
+    i_wl can be an integer or a list of 2 integers used to display the
+    requested spectral channel."""
+    from matplotlib import pyplot as plt
+
+    wl = get_wavelength(instrument, filtname) * 1e6
+
+    if np.isnan(wl.any()):
+        return None
+
+    print(f"\nInstrument: {instrument}, spectral range: {filtname}")
+    print("-----------------------------")
+    print(
+        "spectral coverage: %2.2f - %2.2f µm (step = %2.2f)"
+        % (wl[0], wl[-1], np.diff(wl)[0])
+    )
+
+    one_wl = True
+    if type(i_wl) is list:
+        one_wl = False
+        wl_range = wl[i_wl[0] : i_wl[1]]
+        sp_range = np.arange(i_wl[0], i_wl[1], 1)
+    elif i_wl is None:
+        one_wl = False
+        sp_range = np.arange(len(wl))
+        wl_range = wl
+
+    plt.figure(figsize=(4, 3))
+    plt.title("--- SPECTRAL INFORMATION (IFU)---")
+    plt.plot(wl, label="All spectral channels")
+    if one_wl:
+        plt.plot(
+            np.arange(len(wl))[i_wl],
+            wl[i_wl],
+            "ro",
+            label="Selected (%2.2f µm)" % wl[i_wl],
+        )
+    else:
+        plt.plot(
+            sp_range,
+            wl_range,
+            lw=5,
+            alpha=0.5,
+            label=f"Selected ({wl_range[0]:2.2f}-{wl_range[-1]:2.2f} µm)",
+        )
+    plt.legend()
+    plt.xlabel("Spectral channel")
+    plt.ylabel("Wavelength [µm]")
+    plt.tight_layout()
+
+    if one_wl:
+        output = np.round(wl[i_wl], 2)
+    else:
+        output = np.round(wl_range)
+    return output
